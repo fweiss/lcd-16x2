3c8b993e8ec0e93063d13ca77bd6e4c8e4311dc9
Frank E Weiss <feweiss@gmail.com>
2022-02-07 20:06:50 -0800

    Remove dead code

--- src/MCP23017.hpp
+++ src/MCP23017.hpp
@@ -1,10 +1,7 @@
-#ifndef MCP2307_H
-#define MCP2307_H
-
 /**
  * @file MCP23017.hpp
  * @author feweiss@gmail.com
- * @brief A wrapper around I2CDevice for symbolic access the the MCP23017
+ * @brief A wrapper around I2CDevice for symbolic access to the registers of the MCP23017
  * I2C GPIO port expander.
  * @version 0.1
  * @date 2022-01-29
@@ -12,6 +9,9 @@
  * @copyright Copyright (c) 2022
  * 
  */
+
+#ifndef MCP2307_H
+#define MCP2307_H
 
 #include "I2CDevice.hpp"
 
@@ -27,9 +27,6 @@ class MCP23017 : public I2CDevice {
 public:
     MCP23017(I2C &i2c, uint8_t addr7);
 
-    int readXXX();
-    void backlight(uint8_t color);
-
     enum reg {
         IODIRA, IODIRB,
         IPOLA, IPOLB,
@@ -43,24 +40,6 @@ private:
 };
 
 MCP23017::MCP23017(I2C &i2c, uint8_t addr7) : I2CDevice(i2c, addr7) {
-    setup();
 }
 
-void MCP23017::setup() {
-    // set pullups for buttons on PORTA bits 43210
-    writeRegister(GPPUA, 0x1f);
-    // set output for backlight
-    writeRegister(IODIRB, 0xFE); // A0
-    writeRegister(IODIRA, 0x3f); // B7, B6
-}
-
-int MCP23017::readXXX() {
-    return readRegister(GPIOA);
-}
-
-void MCP23017::backlight(uint8_t color) {
-    writeRegister(OLATB, 0x01);
-    writeRegister(OLATA, color ? 0xc0 : 0x00);
-}
-
-#endif // MCP2307_H
+#endif // MCP23017_H
